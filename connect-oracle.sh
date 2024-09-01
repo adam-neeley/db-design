f51b1e98eab8262ac5134fcff6062437192c6948
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-01 05:03:04 -0700

    clean up

--- connect-oracle.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p bash sshpass
-sshpass -f.pass ssh atn22@nrs-projects-ssh.humboldt.edu -t sqlplus /
--- flake.nix
+++ flake.nix
@@ -1,33 +1,28 @@
 {
   description = "A Nix-flake-based Shell development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
 
   outputs = { self, nixpkgs }:
     let
       supportedSystems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          });
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            # oracle-instantclient # sqliteplus
-            sshpass
-            (pkgs.writeShellScriptBin "connect-oracle"
-              (builtins.readFile ./connect-oracle.sh))
+            sshfs
+            (writeShellScriptBin "run-sql" ''
+              echo "TODO"
+            '')
+            (writeShellScriptBin "mount-humboldt.edu" ''
+              echo "Mounting humboldt.edu server..."
+              sshfs atn22@nrs-projects-ssh.humboldt.edu:/home/atn22 public -o IdentityFile=/home/adam/.ssh/adam@iron
+            '')
           ];
-          # shellHook = "connect-oracle";
-          # shellHook = ''
-          #   nix-shell -p sshpass --run "sshpass -f.pass ssh atn22@nrs-projects-ssh.humboldt.edu -t sqlplus /"
-          # '';
         };
       });
     };
